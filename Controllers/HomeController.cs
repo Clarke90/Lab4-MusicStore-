e0f381bf44f393694ae86b6f03c6ffdc7043c07c
Chad Clarke <chadclarke@bell.net>
2016-11-23 19:35:04 -0500

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Week11.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Location()
+        {
+            // set a message to be displayed in the view
+            ViewBag.Message = "We will show a map here soon.";
+
+            // load the new view
+            return View();
+            //return View("~/Views/Home/artem.cshtml");
+        }
+        public ActionResult Songs()
+        {
+            // set a message to be displayed in the view
+            ViewBag.Message = "Here are some songs.";
+
+            // load the new view
+           // return View();
+            return View("");
+        }
+    }
+}
---
+++ Controllers/SongsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Week11.Controllers
+{
+    public class SongsController : Controller
+    {
+
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: /Songs/Browse
+        public ActionResult Browse()
+        {
+            // create array of hard-coded genres
+            var songs = new string[] { "You Shook Me", "King Of Pain", "City of Blinding Lights" };
+
+            ViewBag.songs = songs;
+            return View();
+        }
+    }
+}
